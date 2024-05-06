295b985899b2b247d2f4d171f1b8250608492f86
Milton <mvindasr@ucenfotec.ac.cr>
2024-05-05 23:55:01 -0600

    Initial commit

---
+++ basic-testing-frontend/src/math.test.js
@@ -0,0 +1,60 @@
+import { expect, it } from 'vitest';
+import { add } from './math';
+
+it('should summarize all number values in an array', () => {
+    // Arrange
+    const numbers = [1, 2];
+    
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers.reduce(
+        (prevValue, curValue) => prevValue + curValue, 0
+    );
+    expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if at least one invalid number is provide', () => {
+    const inputs = ['invalid', 1];
+
+    const result = add(inputs);
+
+    expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an araay of numeric strings values is provided', () => {
+    const numbers = ['1', '2'];
+
+    const result = add(numbers);
+
+    const expectedResult = numbers.reduce(
+        (prevValue, curValue) => +prevValue + +curValue, 0
+    );
+    expect(result).toBe(expectedResult);
+});
+
+it('should yield 0 if an empty array is provided', () => {
+    const numbers = [];
+
+    const result = add(numbers);
+
+    expect(result).toBe(0);
+});
+
+it('should throw an error if no value is passed into the function', () => {
+    const resultFn = () => {
+        add();  
+    };
+    expect(resultFn).toThrow();
+});
+
+it('should throw an error if provided with multiple arguments instead of an array', () => {
+    const num1 = 1;
+    const num2 = 2;
+
+    const resultFn = () => {
+        add(num1, num2);  
+    };
+    expect(resultFn).toThrow(/is not iterable/);
+});
